c6b46bcde58ed6b02ecde5fd748692caa3b9fbd4
Tomi Belan <tomi.belan@gmail.com>
2013-07-03 17:03:14 +0200

    Naimportovana Tomiho pociatocna verzia anketa areny.

---
+++ web/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<title>Anketa arena</title>
+
+<?php if (isset($_SERVER['BADCOOKIE'])) { ?>
+
+<h1>Anketa arena - pristupova brana</h1>
+<form action="#" onsubmit="document.cookie='anketaarenaheslo='+H.value; location.reload(); return false;">
+Ak chces vstupit, povedz pristupovy kod: <input type="password" id="heslo"> <input type="submit" value="OK">
+</form>
+<script>(H=document.getElementById('heslo')).focus();</script>
+
+<?php } else { ?>
+
+<h1>Anketa arena</h1>
+<ul>
+<?php
+$d = scandir('.');
+foreach ($d as $f) if (is_dir($f) && $f[0] != '.') {
+  $o = posix_getpwuid(fileowner($f))['name'];
+  if (preg_match('/^\w+$/', $f.$o)) print "<li><a href='$f'>$f</a> ($o)";
+}
+?>
+</ul>
+
+<?php } ?>
